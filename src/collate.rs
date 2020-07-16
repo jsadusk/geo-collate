03fba3f2627cb69f867db01cd691545c9e379c16
Joe Sadusk <joe@sadusk.com>
2020-07-16 00:38:54 -0700

    Refactored hole mapping from final assembly, and now we have a collate_into, however it doesn't really move values into the new MultiPolygon yet

--- src/collate.rs
+++ src/collate.rs
@@ -43,7 +43,7 @@ where
     T: CoordinateType,
 {
     fn collate(&self) -> CollateResult<MultiPolygon<T>>;
-    //fn collate_into(self) -> MultiPolygon;
+    fn collate_into(self) -> CollateResult<MultiPolygon<T>>;
 }
 
 #[derive(PartialEq)]
@@ -131,178 +131,231 @@ where
     index: usize,
 }
 
+fn get_poly_ranges<T>(polys: &MultiLineString<T>) -> Vec<PolyRange<T>>
+where
+    T: CoordinateType,
+{
+    polys
+        .0
+        .iter()
+        .enumerate()
+        .map(|(index, ls)| {
+            let rect = ls.bounding_rect().unwrap();
+            PolyRange {
+                lower: rect.min().y,
+                upper: rect.max().y,
+                index,
+            }
+        })
+        .collect()
+}
+
+fn get_sweep_lines<T>(ranges: Vec<PolyRange<T>>) -> Vec<T>
+where
+    T: CoordinateType + Numeric,
+{
+    let mut highest_low = ranges.first().unwrap().lower;
+    let mut lowest_high = ranges.first().unwrap().upper;
+
+    let mut sweeps = Vec::<T>::new();
+
+    for range in ranges.iter() {
+        if range.lower >= lowest_high {
+            sweeps.push((lowest_high - highest_low) / T::from_int(2) + highest_low);
+            highest_low = range.lower;
+            lowest_high = range.upper;
+        } else {
+            if range.lower > highest_low {
+                highest_low = range.lower;
+            }
+            if range.upper < lowest_high {
+                lowest_high = range.upper;
+            }
+        }
+    }
+    sweeps.push((lowest_high - highest_low) / T::from_int(2) + highest_low);
+
+    sweeps
+}
+
+fn tie_lines_to_polys<T>(polys: &MultiLineString<T>) -> Vec<TiedLine<T>>
+where
+    T: CoordinateType,
+{
+    let mut lines = Vec::<TiedLine<T>>::new();
+
+    for (index, ls) in polys.0.iter().enumerate() {
+        for line in ls.lines() {
+            lines.push(TiedLine { line, index });
+        }
+    }
+    lines
+}
+
+fn get_poly_hole_map<T>(
+    polys: &MultiLineString<T>,
+) -> CollateResult<(HashMap<usize, usize>, HashSet<usize>)>
+where
+    T: CoordinateType + Numeric,
+{
+    let mut poly_ranges = get_poly_ranges(polys);
+
+    quickersort::sort_by(&mut poly_ranges, &|a, b| {
+        a.lower.partial_cmp(&b.lower).unwrap()
+    });
+
+    let mut sweeps = get_sweep_lines(poly_ranges);
+    quickersort::sort_by(&mut sweeps, &|a, b| a.partial_cmp(&b).unwrap());
+
+    let mut lines = tie_lines_to_polys(polys);
+    quickersort::sort_by(&mut lines, &|a, b| a.miny().partial_cmp(&b.miny()).unwrap());
+
+    let mut valid_lines = BinaryHeap::new();
+    let mut cur_line_iter = lines.iter().peekable();
+
+    let mut hole_of = HashMap::<usize, usize>::new();
+    let mut exteriors = HashSet::<usize>::new();
+
+    for sweep in sweeps {
+        while cur_line_iter.peek() != None {
+            let peeked = &cur_line_iter.peek().unwrap();
+            if peeked.miny() > sweep {
+                break;
+            }
+            let next = cur_line_iter.next().unwrap();
+
+            if next.maxy() >= sweep {
+                valid_lines.push(next);
+            }
+        }
+
+        while !valid_lines.is_empty() && valid_lines.peek().unwrap().maxy() < sweep {
+            valid_lines.pop();
+        }
+
+        if valid_lines.is_empty() {
+            return Err(CollateError::NoValidLinesForSweep);
+        }
+
+        let mut intersections: Vec<SweepIntersection<T>> = valid_lines
+            .iter()
+            .map(|line| {
+                if line.line.start.y == line.line.end.y {
+                    SweepIntersection {
+                        x: line.minx(),
+                        direction: UpDown::Horizontal,
+                        index: line.index,
+                    }
+                } else {
+                    let lefty = if line.line.start.x < line.line.end.x {
+                        line.line.start.y
+                    } else {
+                        line.line.end.y
+                    };
+
+                    let x = line.minx() + (sweep - lefty) * line.line.dx() / line.line.dy();
+
+                    let direction = if line.line.start.y < line.line.end.y {
+                        UpDown::Up
+                    } else {
+                        UpDown::Down
+                    };
+
+                    SweepIntersection {
+                        x,
+                        direction,
+                        index: line.index,
+                    }
+                }
+            })
+            .collect();
+        quickersort::sort_by(
+            &mut intersections,
+            &|a: &SweepIntersection<T>, b: &SweepIntersection<T>| a.x.partial_cmp(&b.x).unwrap(),
+        );
+
+        intersections.dedup();
+
+        let mut poly_stack = Vec::<usize>::new();
+        let mut inside = false;
+
+        for intersection in intersections {
+            let last = poly_stack.last();
+
+            if !inside {
+                match intersection.direction {
+                    UpDown::Up => {
+                        if last == None || intersection.index != *last.unwrap() {
+                            exteriors.insert(intersection.index);
+                        }
+                        inside = true;
+                    }
+                    UpDown::Down => {
+                        return Err(CollateError::HoleWithoutOutline(
+                            sweep.as_float(),
+                            intersection.x.as_float(),
+                        ))
+                    }
+                    UpDown::Horizontal => continue,
+                }
+            } else {
+                match intersection.direction {
+                    UpDown::Up => return Err(CollateError::OutlineInOutline),
+                    UpDown::Down => match poly_stack.last() {
+                        None => return Err(CollateError::EmptyPolyStack),
+                        Some(last) => {
+                            if intersection.index != *last {
+                                if hole_of.contains_key(last) {
+                                    return Err(CollateError::OutlineIsHole);
+                                } else {
+                                    hole_of.insert(intersection.index, *last);
+                                }
+                            }
+                            inside = false;
+                        }
+                    },
+                    UpDown::Horizontal => continue,
+                }
+            }
+
+            if last != None && intersection.index == *poly_stack.last().unwrap() {
+                poly_stack.pop();
+            } else {
+                poly_stack.push(intersection.index);
+            }
+        }
+    }
+
+    Ok((hole_of, exteriors))
+}
+
 impl<T> Collate<T> for MultiLineString<T>
 where
     T: CoordinateType + Numeric + fmt::Display + fmt::Debug,
 {
     fn collate(&self) -> CollateResult<MultiPolygon<T>> {
-        let mut poly_ranges: Vec<PolyRange<T>> = self
-            .0
-            .iter()
-            .enumerate()
-            .map(|(index, ls)| {
-                let rect = ls.bounding_rect().unwrap();
-                PolyRange {
-                    lower: rect.min().y,
-                    upper: rect.max().y,
-                    index,
-                }
-            })
-            .collect();
+        let (hole_of, exteriors) = get_poly_hole_map(self)?;
 
-        quickersort::sort_by(&mut poly_ranges, &|a, b| {
-            a.lower.partial_cmp(&b.lower).unwrap()
-        });
-        let mut highest_low = poly_ranges.first().unwrap().lower;
-        let mut lowest_high = poly_ranges.first().unwrap().upper;
-
-        let mut sweeps = Vec::<T>::new();
-
-        for poly_range in poly_ranges.iter() {
-            if poly_range.lower >= lowest_high {
-                sweeps.push((lowest_high - highest_low) / T::from_int(2) + highest_low);
-                highest_low = poly_range.lower;
-                lowest_high = poly_range.upper;
-            } else {
-                if poly_range.lower > highest_low {
-                    highest_low = poly_range.lower;
-                }
-                if poly_range.upper < lowest_high {
-                    lowest_high = poly_range.upper;
-                }
-            }
-        }
-        sweeps.push((lowest_high - highest_low) / T::from_int(2) + highest_low);
-
-        let mut lines = Vec::<TiedLine<T>>::new();
-
-        for (index, ls) in self.0.iter().enumerate() {
-            for line in ls.lines() {
-                lines.push(TiedLine { line, index });
-            }
+        let mut polys = HashMap::<usize, Vec<LineString<T>>>::new();
+        for outer_index in exteriors {
+            polys.insert(outer_index, Vec::new());
         }
 
-        quickersort::sort_by(&mut lines, &|a, b| a.miny().partial_cmp(&b.miny()).unwrap());
-
-        quickersort::sort_by(&mut sweeps, &|a, b| a.partial_cmp(&b).unwrap());
-
-        let mut valid_lines = BinaryHeap::new();
-        let mut cur_line_iter = lines.iter().peekable();
-
-        let mut hole_of = HashMap::<usize, usize>::new();
-        let mut exteriors = HashSet::<usize>::new();
-
-        for sweep in sweeps {
-            while cur_line_iter.peek() != None {
-                let peeked = &cur_line_iter.peek().unwrap();
-                if peeked.miny() > sweep {
-                    break;
-                }
-                let next = cur_line_iter.next().unwrap();
-
-                if next.maxy() >= sweep {
-                    valid_lines.push(next);
-                }
-            }
-
-            while !valid_lines.is_empty() && valid_lines.peek().unwrap().maxy() < sweep {
-                valid_lines.pop();
-            }
-
-            if valid_lines.is_empty() {
-                return Err(CollateError::NoValidLinesForSweep);
-            }
-
-            let mut intersections: Vec<SweepIntersection<T>> = valid_lines
-                .iter()
-                .map(|line| {
-                    if line.line.start.y == line.line.end.y {
-                        SweepIntersection {
-                            x: line.minx(),
-                            direction: UpDown::Horizontal,
-                            index: line.index,
-                        }
-                    } else {
-                        let lefty = if line.line.start.x < line.line.end.x {
-                            line.line.start.y
-                        } else {
-                            line.line.end.y
-                        };
-
-                        let x = line.minx() + (sweep - lefty) * line.line.dx() / line.line.dy();
-
-                        let direction = if line.line.start.y < line.line.end.y {
-                            UpDown::Up
-                        } else {
-                            UpDown::Down
-                        };
-
-                        SweepIntersection {
-                            x,
-                            direction,
-                            index: line.index,
-                        }
-                    }
-                })
-                .collect();
-            quickersort::sort_by(
-                &mut intersections,
-                &|a: &SweepIntersection<T>, b: &SweepIntersection<T>| {
-                    a.x.partial_cmp(&b.x).unwrap()
-                },
-            );
-
-            intersections.dedup();
-
-            let mut poly_stack = Vec::<usize>::new();
-            let mut inside = false;
-
-            for intersection in intersections {
-                let last = poly_stack.last();
-
-                if !inside {
-                    match intersection.direction {
-                        UpDown::Up => {
-                            if last == None || intersection.index != *last.unwrap() {
-                                exteriors.insert(intersection.index);
-                            }
-                            inside = true;
-                        }
-                        UpDown::Down => {
-                            return Err(CollateError::HoleWithoutOutline(
-                                sweep.as_float(),
-                                intersection.x.as_float(),
-                            ))
-                        }
-                        UpDown::Horizontal => continue,
-                    }
-                } else {
-                    match intersection.direction {
-                        UpDown::Up => return Err(CollateError::OutlineInOutline),
-                        UpDown::Down => match poly_stack.last() {
-                            None => return Err(CollateError::EmptyPolyStack),
-                            Some(last) => {
-                                if intersection.index != *last {
-                                    if hole_of.contains_key(last) {
-                                        return Err(CollateError::OutlineIsHole);
-                                    } else {
-                                        hole_of.insert(intersection.index, *last);
-                                    }
-                                }
-                                inside = false;
-                            }
-                        },
-                        UpDown::Horizontal => continue,
-                    }
-                }
-
-                if last != None && intersection.index == *poly_stack.last().unwrap() {
-                    poly_stack.pop();
-                } else {
-                    poly_stack.push(intersection.index);
-                }
-            }
+        for (inner_index, outer_index) in hole_of {
+            let inner = polys.get_mut(&outer_index).unwrap();
+            inner.push(self.0[inner_index].clone());
         }
+
+        Ok(MultiPolygon::from(
+            polys
+                .into_iter()
+                .map(|(outer_index, inner)| Polygon::new(self.0[outer_index].clone(), inner))
+                .collect::<Vec<Polygon<T>>>(),
+        ))
+    }
+
+    fn collate_into(self) -> CollateResult<MultiPolygon<T>> {
+        let (hole_of, exteriors) = get_poly_hole_map(&self)?;
 
         let mut polys = HashMap::<usize, Vec<LineString<T>>>::new();
         for outer_index in exteriors {
