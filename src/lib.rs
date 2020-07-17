a570ce53407a72cc89e43de6bf9ae9fffe138f8d
Joe Sadusk <joe@sadusk.com>
2020-07-16 23:16:18 -0700

    Moved tests to their own file

--- src/lib.rs
+++ src/lib.rs
@@ -2,9 +2,8 @@ extern crate geo;
 extern crate quickersort;
 
 pub mod collate;
-pub mod fslope;
 pub mod numeric;
+pub mod test;
 
 pub use crate::collate::*;
-pub use crate::fslope::*;
 pub use crate::numeric::*;
